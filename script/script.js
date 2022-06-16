0ff8151a5e377b587fbf8831a26c16a680a66c54
stecala <ste.cala123@gmail.com>
2022-06-16 15:47:02 +0200

    bonus inventato da me

--- script/script.js
+++ script/script.js
@@ -1,36 +1,44 @@
 const row = document.getElementById('row');
-
-// creo ciclo che va da 1 a 100
-for(let i=1; i<=100; i++){
-    const colToAppend = document.createElement('div');
-    colToAppend.classList.add('col','height','fs-2','fw-bold','pt-4','me-3');
-    // controllo che sia divisibile per 3 e 5
-    if(((i%3)==0) && ((i%5)==0))
-    {
-        console.log('FizzBuzz');
-        colToAppend.innerHTML = "FizzBuzz";
-        colToAppend.classList.add('bg-danger');
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+    const userNumber = parseInt(document.getElementById('number').value);
+    if (isNaN(userNumber) == true) {
+        document.getElementById('wrong').classList.remove('d-none');
     }
-    else{
-        // controllo che sia divisibile per 3
-        if((i%3)==0)
-        {
-            console.log('Fizz');
-            colToAppend.innerHTML = "Fizz";
-            colToAppend.classList.add('bg-success');
-        }
-        // controllo che sia divisibile per 5
-        else if ((i%5)==0) {
-            console.log('Buzz');
-            colToAppend.innerHTML = "Buzz";
-            colToAppend.classList.add('bg-warning');
-        }
-        // zona di esclusione 
-        else{
-            console.log(i);
-            colToAppend.innerHTML = i;
-            colToAppend.classList.add('bg-primary');
+    else {
+        document.getElementById('wrong').classList.add('d-none');
+        // creo ciclo che va da 1 a 100
+        for (let i = userNumber; i <= (userNumber + 100); i++) {
+            document.getElementById('row').classList.remove('d-none');
+            const colToAppend = document.createElement('div');
+            colToAppend.classList.add('col', 'height', 'fs-2', 'fw-bold', 'pt-4', 'me-3');
+            // controllo che sia divisibile per 3 e 5
+            if (((i % 3) == 0) && ((i % 5) == 0)) {
+                console.log('FizzBuzz');
+                colToAppend.innerHTML = "FizzBuzz";
+                colToAppend.classList.add('bg-danger');
+            }
+            else {
+                // controllo che sia divisibile per 3
+                if ((i % 3) == 0) {
+                    console.log('Fizz');
+                    colToAppend.innerHTML = "Fizz";
+                    colToAppend.classList.add('bg-success');
+                }
+                // controllo che sia divisibile per 5
+                else if ((i % 5) == 0) {
+                    console.log('Buzz');
+                    colToAppend.innerHTML = "Buzz";
+                    colToAppend.classList.add('bg-warning');
+                }
+                // zona di esclusione 
+                else {
+                    console.log(i);
+                    colToAppend.innerHTML = i;
+                    colToAppend.classList.add('bg-primary');
+                }
+            }
+            row.append(colToAppend);
         }
     }
-    row.append(colToAppend);
-}
+})
