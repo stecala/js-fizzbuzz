db284885fdbdd64d384528e29811442a94ace83a
stecala <ste.cala123@gmail.com>
2022-06-16 14:56:36 +0200

    fine bonus 2

--- script/script.js
+++ script/script.js
@@ -3,12 +3,13 @@ const row = document.getElementById('row');
 // creo ciclo che va da 1 a 100
 for(let i=1; i<=100; i++){
     const colToAppend = document.createElement('div');
-    colToAppend.classList.add('col-2','text-white','height');
+    colToAppend.classList.add('col-2','height','fs-2','fw-bold','pt-4','me-3');
     // controllo che sia divisibile per 3 e 5
     if(((i%3)==0) && ((i%5)==0))
     {
         console.log('FizzBuzz');
         colToAppend.innerHTML = "FizzBuzz";
+        colToAppend.classList.add('bg-danger');
     }
     else{
         // controllo che sia divisibile per 3
@@ -16,16 +17,19 @@ for(let i=1; i<=100; i++){
         {
             console.log('Fizz');
             colToAppend.innerHTML = "Fizz";
+            colToAppend.classList.add('bg-success');
         }
         // controllo che sia divisibile per 5
         else if ((i%5)==0) {
             console.log('Buzz');
             colToAppend.innerHTML = "Buzz";
+            colToAppend.classList.add('bg-warning');
         }
         // zona di esclusione 
         else{
             console.log(i);
             colToAppend.innerHTML = i;
+            colToAppend.classList.add('bg-primary');
         }
     }
     row.append(colToAppend);
