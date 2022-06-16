5d835d3eb0aa8bc95c84d45a2f1a2c4f90c95ada
stecala <ste.cala123@gmail.com>
2022-06-16 14:34:48 +0200

    bonus 1

--- script/script.js
+++ script/script.js
@@ -1,23 +1,32 @@
+const row = document.getElementById('row');
+
 // creo ciclo che va da 1 a 100
 for(let i=1; i<=100; i++){
+    const colToAppend = document.createElement('div');
+    colToAppend.classList.add('col-2','text-white','height');
     // controllo che sia divisibile per 3 e 5
     if(((i%3)==0) && ((i%5)==0))
     {
         console.log('FizzBuzz');
+        colToAppend.innerHTML = "FizzBuzz";
     }
     else{
         // controllo che sia divisibile per 3
         if((i%3)==0)
         {
             console.log('Fizz');
+            colToAppend.innerHTML = "Fizz";
         }
         // controllo che sia divisibile per 5
         else if ((i%5)==0) {
-            console.log('Buzz');   
+            console.log('Buzz');
+            colToAppend.innerHTML = "Buzz";
         }
         // zona di esclusione 
         else{
             console.log(i);
+            colToAppend.innerHTML = i;
         }
     }
+    row.append(colToAppend);
 }
