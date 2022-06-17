02361835a3eb0e7ce0242db8c3053144897a1bc6
stecala <ste.cala123@gmail.com>
2022-06-17 09:40:59 +0200

    gap no gutter

--- script/script.js
+++ script/script.js
@@ -12,7 +12,7 @@ btn.addEventListener('click', function () {
         for (let i = userNumber; i <= (userNumber + 100); i++) {
             document.getElementById('row').classList.remove('d-none');
             const colToAppend = document.createElement('div');
-            colToAppend.classList.add('col', 'height', 'fs-2', 'fw-bold', 'pt-4', 'me-3');
+            colToAppend.classList.add('col', 'height', 'fs-2', 'fw-bold', 'pt-4');
             // controllo che sia divisibile per 3 e 5
             if (((i % 3) == 0) && ((i % 5) == 0)) {
                 console.log('FizzBuzz');
